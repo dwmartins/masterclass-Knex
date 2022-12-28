db3a2e9eb2d79c5b619a19edfb1faf74a988ee28
Douglas Martins <douglas5422@outlook.com>
2022-12-28 11:59:27 -0300

    Update users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,30 @@ const knex = require('../database');
 module.exports = {
     async index(req, res) {
         const results = await knex('users')
-        
         return res.json(results);
+    },
+    async create(req, res, next) {
+
+        try { 
+            const { username } = req.body;
+            await knex('users').insert({
+                username
+            })
+
+            return res.status(201).send()
+            
+        } catch(error) {
+            next(error)
+        }
+    },
+    async update(req, res, next) {
+        try {
+            const { username } = req.body;
+            const { id } = req.params
+
+            await knex('users').update({username}).where({ id })
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 const UserController = require('./controllers/UserController');
 
-app.get('/users', UserController.index);
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.create);
+routes.put('/users/:id', UserController.update);
 
 module.exports = routes;
